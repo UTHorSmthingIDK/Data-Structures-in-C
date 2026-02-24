8624064bdb2b98b694d919ae52f523503df9289b
UTHorSmtingIDK <ntinosgatsios@gmail.com>
2026-02-24 19:04:51 +0200

    idk

--- generic_Hash_Map.c
+++ generic_Hash_Map.c
@@ -1,13 +1,3 @@
-/*
-includes values and keys
-den epitrepontai dipla keys
-values can be repeated
-me taxinomish kalitera
-fast search
-
-
-index = hash(key) % array_size
-where hash is :
-hash = hash * 31 + ASCII(character)
-
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
