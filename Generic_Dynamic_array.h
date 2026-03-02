5cfef8f21ba5858e4b477fd5ea3b289fba6e8d14
UTHorSmtingIDK <ntinosgatsios@gmail.com>
2026-03-02 23:39:40 +0200

    fixed some issues

--- Generic_Dynamic_array.h
+++ Generic_Dynamic_array.h
@@ -12,7 +12,8 @@ gen_dyn_arr* create_gen_dyn_arr(size_t max_size, size_t element_size);
 void delete_gen_dyn_arr(gen_dyn_arr* gda);
 
 void add_element(gen_dyn_arr* gda, size_t pos, void* element, size_t element_size);
-gen_dyn_arr* remove_element(gen_dyn_arr* gda, size_t pos);
+gen_dyn_arr* resize(gen_dyn_arr* gda, size_t new_max_size);
+void remove_element_in_place(gen_dyn_arr* gda, size_t pos);
 
 void fill_arr(gen_dyn_arr* gda, size_t start, size_t end, void* element, size_t element_size);
 
@@ -22,5 +23,5 @@ void bubble_sort_gen_dyn_arr(gen_dyn_arr* gda, int mode, int (*cmp)(const void*,
 int cmp_int(const void* a, const void* b);
 int cmp_float(const void* a, const void* b);
 
-size_t linear_search_gen_dyn_arr(gen_dyn_arr* gda, void* element, size_t element_size);
-size_t binary_search_gen_dyn_arr(gen_dyn_arr* gda, void* element, size_t element_size);
+size_t linear_search_gen_dyn_arr(gen_dyn_arr* gda, int (*cmp)(const void*, const void*), void* element, size_t element_size);
+size_t binary_search_gen_dyn_arr(gen_dyn_arr* gda, int (*cmp)(const void*, const void*), void* element);
