87eee978bbb74c4ed588df936d4c7e5a5a4aa6ae
UTHorSmtingIDK <ntinosgatsios@gmail.com>
2026-03-02 23:39:13 +0200

    fixed some issues

--- Generic_Dynamic_array.c
+++ Generic_Dynamic_array.c
@@ -1,6 +1,6 @@
 #include "Generic_Dynamic_array.h"
 
-/*int main(void){
+int main(void){
 
     gen_dyn_arr* gda = create_gen_dyn_arr(6, 4);
 
@@ -34,14 +34,14 @@
     }
 
     return 0;
-}*/
+}
 
 
 gen_dyn_arr* create_gen_dyn_arr(size_t max_size, size_t element_size){
     gen_dyn_arr* gda = malloc(sizeof(gen_dyn_arr));
     gda->element_size = element_size;
     gda->max_size = max_size;
-    gda->data = malloc(gda->max_size);
+    gda->data = malloc(gda->max_size * element_size);
 }
 
 void delete_gen_dyn_arr(gen_dyn_arr* gda){
@@ -61,21 +61,21 @@ void add_element(gen_dyn_arr* gda, size_t pos, void* element, size_t element_siz
     memcpy((char*)gda->data + pos * gda->element_size, element, gda->element_size);
 }
 
-gen_dyn_arr* remove_element(gen_dyn_arr* gda, size_t pos){
-    if (pos < 0 || pos >= gda->max_size){
+
+void remove_element_in_place(gen_dyn_arr* gda, size_t pos) {
+    if (pos >= gda->max_size || pos < 0){
         printf("This action is not posible.(pos is out of bounds)");
-        return NULL;
+        return;
     }
-    gen_dyn_arr* gda1 = create_gen_dyn_arr(gda->max_size - 1, gda->element_size);
-    for (size_t i = 0; i < gda->max_size - 1; i++){
-        if (i == pos){
-            memcpy((char*)gda1->data + i * gda1->element_size, (char*)gda->data + (i + 1) * gda->element_size, gda1->element_size);//add to da1 the next element in the data
-        } else {
-            memcpy((char*)gda1->data + i * gda1->element_size, (char*)gda->data + i * gda->element_size, gda1->element_size);//copy the element of da into da1
-        }
+    if (pos < gda->max_size - 1) {  // If it's not the last element, shift everything after it one spot to the left
+        char* dest = (char*)gda->data + (pos * gda->element_size);
+        char* src = (char*)gda->data + ((pos + 1) * gda->element_size);
+        size_t bytes_to_move = (gda->max_size - pos - 1) * gda->element_size;
+        memmove(dest, src, bytes_to_move);
     }
-    delete_gen_dyn_arr(gda);
+    gda->max_size--;
 }
+
 
 void fill_arr(gen_dyn_arr* gda, size_t start, size_t end, void* element, size_t element_size){
     if (start > end || start < 0 || start >= gda->max_size || end < 0 || end >= gda->max_size){
@@ -96,6 +96,13 @@ void get_element(gen_dyn_arr* gda, void* val, size_t pos, size_t element_size){
         printf("Warning: Type of [da->data] is not compatible with Type of [val]");
     }
     memcpy(val, (char*)gda->data + pos * gda->element_size, element_size);
+}
+
+gen_dyn_arr* resize(gen_dyn_arr* gda, size_t new_max_size){
+    gen_dyn_arr* gda1 = create_gen_dyn_arr(new_max_size, gda->element_size);
+    for (int i = 0; i < new_max_size; i++){
+        memcpy((char*)gda1->data + i * gda->element_size, (char*)gda->data + i * gda->element_size, gda->element_size);
+    }
 }
 
 void bubble_sort_gen_dyn_arr(gen_dyn_arr* gda, int mode, int (*cmp)(const void*, const void*)) {  //we pass a function pointer as an argument to the Buble sort
@@ -141,39 +148,34 @@ int cmp_float(const void* a, const void* b){
 }
 
 
-size_t linear_search_gen_dyn_arr(gen_dyn_arr* gda, void* element, size_t element_size){
+size_t linear_search_gen_dyn_arr(gen_dyn_arr* gda, int (*cmp)(const void*, const void*), void* element, size_t element_size){
     if (element_size != gda->element_size){
         printf("Types of [element] and [gda->data] are incompatible\n");
         return -1;
     }
     for(size_t i = 0; i < gda->max_size; i++){
-        if (!memcmp(element, (char*)gda->data + i * gda->element_size, element_size)){
+        if (!cmp((char*)gda->data + i * element_size, element)){
             return i;
         }
     }
     return -1;
 }
 
-size_t binary_search_gen_dyn_arr(gen_dyn_arr* gda, void* element, size_t element_size){
-    if (element_size != gda->element_size){
-        printf("Types of [element] and [gda->data] are incompatible\n");
-        return -1;
-    }
+
+size_t binary_search_gen_dyn_arr(gen_dyn_arr* gda, int (*cmp)(const void*, const void*), void* element) {
     size_t bot = 0;
-    size_t top = gda->max_size;
-    size_t mid = (top - bot)/2;
-    int flag, flag1;
-    while((flag = memcmp(element, (char*)gda->data + mid * gda->element_size, element_size)) && (flag1 = top > bot)){
-        if (flag < 0){  //element < gda->data
+    size_t top = gda->max_size - 1;
+
+    while (bot <= top) {
+        size_t mid = bot + (top - bot) / 2;
+        void* mid_ptr = (char*)gda->data + (mid * gda->element_size);   // Cast to char* for byte-accurate offsetting
+        int res = cmp(mid_ptr, element);    //used in the conditions
+        if (res == 0) return mid;   //element found
+        if (res < 0) bot = mid + 1;
+        else {
+            if (mid == 0) break; // Prevent underflow of size_t
             top = mid - 1;
-            mid = (top + bot)/2;
-        } else {    //element > gda->data
-            bot = mid + 1;
-            mid = (top + bot)/2;
         }
     }
-    if (!flag1){
-        return mid;
-    }
-    return -1;
+    return -1; // Not found
 }
